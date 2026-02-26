9b65fc08c0eef20ad95a7fb29819d1f06d30b7d5
PanMartin <panmartinrobert@163.com>
2026-02-26 13:58:53 +0800

    if else练习
    练习上传

---
+++ if.else练习.py
@@ -0,0 +1,29 @@
+# # # #1，需求1：根据用户输入的数字，判断该数字是奇数还是偶数。
+# # num = int(input("请输入一个要检测的数字"))
+# # if num % 2 == 0:
+# #     print(f"该数字{num}是偶数")
+# # else:
+# #     print(f"该数字{num}是奇数")
+#
+# # 2，需求2：根据用户输入的年龄，判断该用户是否已经成年（>=18，成年；否则，未成年）。
+# age = int(input("请输入您的年龄:"))
+# if age >= 18:
+#     print("恭喜您，你现在可以合法看片去网吧了")
+# else:
+#     print("小屁孩")
+
+# 3．需求3：根据用户输入的数字，判断该数字是正数还是负数（不考虑0）。
+# num = int(input("Enter a number: "))
+# if (num < 0) and num != 0:
+#     print(f"{num} 是负数")
+# if (num > 0) and (num != 0):
+# #     print(f"{num} 是正数")
+#
+#
+# # 4，需求4：根据用户输入的考试分数，判断该分数是否及格了（大于等于60就是及格了）。
+# score = int(input("Enter your score: "))
+# if score >= 60:
+#     print("恭喜你合格了.老底")
+# else:
+#     print("没合格，但是恭喜你挂科了")
+#
